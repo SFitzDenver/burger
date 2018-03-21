52b5d3b77cb1397b2345a2af59e4f12164146dcf
Sherry Fitzpatrick <sfitz3141@gmail.com>
2018-03-20 18:06:36 -0600

    Add burger button working

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,22 @@
-// $function() {
-// 	// POST request
-// 	$("")
+$(function() {
+	// POST request
+	$(".create-form").on("submit", function(event) {
+		event.preventDefault();
 
-// 	$()
-// }
+		var newBurger = {
+			name: $("#burger").val().trim(),
+		};
+
+		$.ajax("/api/burgers", {
+			type: "POST",
+			data: newBurger
+		}).then(
+		function() {
+			console.log("created new burger");
+			location.reload();
+		}
+		);
+		});
+	});
+
+
